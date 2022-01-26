044e228333a426c3dc3fe642c1bd3b43ec90f569
mitchellhein25 <mitchelldalehein25@gmail.com>
2022-01-26 06:51:46 -0600

    Improved rehab Modal layout

--- bcs-realestate/components/rehabDetailedListView/rehabDetailedListView.js
+++ bcs-realestate/components/rehabDetailedListView/rehabDetailedListView.js
@@ -1,18 +1,34 @@
 import React from 'react'
+import Image from 'next/image'
 import rehabDetailedListViewStyles from './rehabDetailedListView.module.css'
 
 export default function RehabDetailedListView() {
 
     const itemList = [
-        { name: "Item 1 Name", photo: "Item 1 Photo", price: "Item 1 Price", description: "Item 1 Description" },
-        { name: "Item 2 Name", photo: "Item 2 Photo", price: "Item 2 Price", description: "Item 2 Description" }
+        { name: "Item 1 Name", photo: "/images/gregHeadshot.jpg", price: "Item 1 Price", description: "Item 1 Description" },
+        { name: "Item 2 Name", photo: "/images/gregHeadshot.jpg", price: "Item 2 Price", description: "Item 2 Description" },
+        { name: "Item 2 Name", photo: "/images/gregHeadshot.jpg", price: "Item 2 Price", description: "Item 2 Description" },
+        { name: "Item 2 Name", photo: "/images/gregHeadshot.jpg", price: "Item 2 Price", description: "Item 2 Description" }
     ]
 
     return (
         <div className={rehabDetailedListViewStyles.container}>
             {itemList.map((li, idx) => (
-                <button key={idx} className={rehabDetailedListViewStyles.listItem} onClick={() => setOpen(true)}>
-                    <p>{li.name}</p>
+                <button key={idx} className={rehabDetailedListViewStyles.listItem}>
+                    <div className={rehabDetailedListViewStyles.detailContainer}>
+                        <h3>{li.name}</h3>
+                        <p>{li.description}</p>
+                        <h4>Price: {li.price}</h4>
+                    </div>
+                    <div className={rehabDetailedListViewStyles.imageContainer}>
+                        <Image
+                            src={li.photo}
+                            alt={li.description}
+                            width={150}
+                            height={150}
+                            className={rehabDetailedListViewStyles.imageRounded}
+                        />
+                    </div>
                 </button>
             ))}
         </div>
