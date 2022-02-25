0a3ddbb492ebad8ec636540cd74dbcf679a014a6
mitchellhein25 <mitchelldalehein25@gmail.com>
2022-02-25 06:35:46 -0600

    Created list of 5 Banks and completed bank page initial design

--- bcs-realestate/components/detailedListView/detailedListView.js
+++ bcs-realestate/components/detailedListView/detailedListView.js
@@ -6,37 +6,43 @@ export default function DetailedListView({ itemList }) {
     return (
         <div className={detailedListViewStyles.container}>
             {itemList.map((li, idx) => (
-                <button key={idx} className={detailedListViewStyles.listItem}>
+                <div key={idx} className={detailedListViewStyles.listItem}>
                     <div className={detailedListViewStyles.detailContainer}>
-                        <h3>{li.name}</h3>
-                        {li.details.phone ?
+                        <h3 className={detailedListViewStyles.liHeader}>{li.name}</h3>
+                            {li.details.phone ?
                             <p className={detailedListViewStyles.detailItem}>
-                                <b>Phone</b>: {li.details.phone}</p>
+                                    <b>Phone</b>: {li.details.phone}</p>
+                                : null
+                            }
+                        {li.details.address ?
+                            <div className={detailedListViewStyles.detailItem + " " + detailedListViewStyles.toRow}>
+                                <p className={detailedListViewStyles.addressNoMargin}><b>Address</b>:&nbsp;</p>
+                                <p className={detailedListViewStyles.addressNoMargin} >{li.details.address.addressLine1}<br></br>
+                                   {li.details.address.addressLine2}</p>
+                            </div>
                             : null
                         }
                         {li.details.website ?
-                            <p className={detailedListViewStyles.detailItem}>
-                                <b>Website</b>: {li.details.website}</p>
-                            : null
-                        }
-                        {li.details.address ?
-                            <p className={detailedListViewStyles.detailItem}>
-                                <b>Address</b>: {li.details.address}</p>
+                            <p className={detailedListViewStyles.website}>
+                                <a target="_blank" href={li.details.website}>{li.details.websiteText}</a></p>
                             : null
                         }
                     </div>
-                    {li.photo ?
+                    <div className={detailedListViewStyles.rightContainer}>
+                    {li.image ?
                         <div className={detailedListViewStyles.imageContainer}>
                             <Image
-                                src={li.photo}
-                                alt={li.photoDesc + " logo"}
-                                width={150}
-                                height={150}
+                                src={li.image.photo}
+                                alt={li.image.photoDesc + " logo"}
+                                width={li.image.photoWidth}
+                                height={li.image.photoHeight}
                                 className={detailedListViewStyles.imageRounded}
                             />
                         </div> : null
                     }
-                </button>
+                    
+                    </div>
+                </div>
             ))}
         </div>
     )
--- bcs-realestate/pages/banks.js
+++ bcs-realestate/pages/banks.js
@@ -1,54 +1,110 @@
 import MainLayout from '../components/mainLayout/mainLayout'
 import DetailedListView from '../components/detailedListView/detailedListView'
+import bankStyles from '../styles/Banks.module.css'
 
 export default function Banks() {
 
     const banksList = [{
         name: "City Bank",
-        photo: "/images/city-bank.png",
-        photoDesc: "City Bank logo",
+        image: {
+            photo: "/images/city-bank.png",
+            photoDesc: "City Bank logo",
+            photoWidth: 125,
+            photoHeight: 125,
+        },
         details: {
             phone: "(979) 846-7844",
             website: "https://www.city.bank/locations/details/mortgage-college-station-branch",
-            address: "1103 University Dr. East, Ste. 100 College Station, TX 77840"
+            websiteText: "Go to City Bank Website",
+            //address: {
+            //    addressLine1: "1103 University Dr. East, Ste. 100",
+            //    addressLine2: "College Station, TX 77840"
+            //}
         }
     },
-    //{
-    //    name: "1st Financial Bank",
-    //    photo: "/images/gregHeadshot.jpg",
-    //    details: {
-    //        "phone": "Item 1 Price",
-    //        "website": "Item 1 Description"
-    //    }
-    //},  
-    //{
-    //    name: "Extaco",
-    //    photo: "/images/gregHeadshot.jpg",
-    //    details: {
-    //        "phone": "Item 1 Price",
-    //        "website": "Item 1 Description"
-    //    }
-    //},
-    //{
-    //    name: "Guaranty Bank & Trust",
-    //    photo: "/images/gregHeadshot.jpg",
-    //    details: {
-    //        "phone": "Item 1 Price",
-    //        "website": "Item 1 Description"
-    //    }
-    //},
-    //{
-    //    name: "USAA",
-    //    photo: "/images/gregHeadshot.jpg",
-    //    details: {
-    //        "phone": "Item 1 Price",
-    //        "website": "Item 1 Description"
-    //    }
-    //}
+    {
+        name: "1st Alliance Mortgage",
+        image: {
+            photo: "/images/1stAM-Logo.jpg",
+            photoDesc: "1st Alliance logo",
+            photoWidth: 250,
+            photoHeight: 60,
+        },
+        details: {
+            phone: "(713) 626-5700",
+            website: "https://1stalliancemortgage.com/",
+            websiteText: "Go to 1st Alliance Mortgage Website",
+            //address: {
+            //    addressLine1: "750 William D. Fitch Pkwy, Suite 330",
+            //    addressLine2: "College Station, Texas 77845"
+            //}
+        }
+    },  
+    {
+        name: "Extraco",
+        image: {
+            photo: "/images/extraco-logo.svg",
+            photoDesc: "Extraco logo",
+            photoWidth: 250,
+            photoHeight: 60,
+        },
+        details: {
+            phone: "(979) 823-8810",
+            website: "https://www.extracobanks.com/",
+            websiteText: "Go to Extraco Bank Website",
+            //address: {
+            //    addressLine1: "2706 S. Texas Ave",
+            //    addressLine2: "Bryan, TX 77802"
+            //}
+        }
+    },
+    {
+        name: "Guaranty Bank & Trust",
+        image: {
+            photo: "/images/guaranty-logo.svg",
+            photoDesc: "Guaranty Bank & Trust logo",
+            photoWidth: 250,
+            photoHeight: 60,
+        },
+        details: {
+            phone: "(979) 485-5880",
+            website: "https://answers.gnty.com/index.html?query=locations+near+me&referrerPageUrl=https%3A%2F%2Fwww.gnty.com%2F",
+            websiteText: "Go to Guaranty Bank & Trust Website",
+            //address: {
+            //    addressLine1: "2700 Earl Rudder Freeway Suite 1000",
+            //    addressLine2: "College Station, TX 77845"
+            //}
+        }
+    },
+    {
+        name: "USAA (Military only)",
+        image: {
+            photo: "/images/usaa-logo.png",
+            photoDesc: "USAA logo",
+            photoWidth: 300,
+            photoHeight: 150,
+        },
+        details: {
+            phone: "(800) 531-0341",
+            website: "https://www.usaa.com/inet/wc/bank-real-estate-mortgage-loans?wa_ref=pub_global_banking_home_mortgages_mortgages_main",
+            websiteText: "Go to USAA Website",
+            //address: {
+            //    addressLine1: "2706 S. Texas Ave",
+            //    addressLine2: "Bryan, TX 77802"
+            //}
+        }
+    }
     ]
+
+    const subHeader = `We know it can be hard to filter through the long list of banks you get by searching Google, so here 
+                        is a list of some of our favorite and most trusted banks in the Bryan/College Station area!`
 
     return (
         <MainLayout>
+            <div className={bankStyles.headerContainer}>
+                <h1 className={bankStyles.mainHeader}>B/CS Banks</h1>
+                <p className={bankStyles.subheader}>{subHeader}</p>
+            </div>
             <DetailedListView itemList={banksList}></DetailedListView>
         </MainLayout>
     )
